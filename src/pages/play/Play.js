7a6b2c471259afaf7375c31049a28b4e452c06fd
Mahesh Sale <maheshsale333@gmail.com>
2023-11-14 19:07:25 +0530

    Added basic features

--- src/pages/play/Play.js
+++ src/pages/play/Play.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import './Play.css'
-import Cube from './images/cubes_gambling.png'
+// import Cube from './images/dice_1.png'
 
 function Play(){
+      const [selectedNumber, setSelectedNumber] = useState(null);
+      const [selectedDice, setSelectedDice] = useState(null);
+
+      const [cube, setCube] = useState('images/dice_1.png');
+      
+      let diceArray = ['dice_1', 'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6'];
+      
+      const handleChange = (event) => {
+            setSelectedNumber(parseInt(event.target.value));
+      }
+
+      const getDice = (index) => {
+            let imgPath = 'images/' + diceArray[index] + '.png';
+            return imgPath;
+      }
+
+      const rollDice = () => {
+            let randomIndex = Math.floor(Math.random() * 6);
+            setCube(getDice(randomIndex));
+            setSelectedDice(randomIndex + 1);
+      }
       return(
             <div className="container1">
                   <div className="header">
@@ -11,24 +33,25 @@ function Play(){
                         </div>
                         <div className="select-number">
                               <div className="buttons">
-                                    <input type="radio" name="selected-number" value="1" id="one" />
+                                    <input type="radio" name="selected-number" value="1" id="one" onChange={handleChange}/>
                                     <label htmlFor="one">1</label>
-                                    <input type="radio" name="selected-number" value="2" id="two" />
+                                    <input type="radio" name="selected-number" value="2" id="two" onChange={handleChange}/>
                                     <label htmlFor="two">2</label>
-                                    <input type="radio" name="selected-number" value="3" id="three" />
+                                    <input type="radio" name="selected-number" value="3" id="three" onChange={handleChange}/>
                                     <label htmlFor="three">3</label>
-                                    <input type="radio" name="selected-number" value="4" id="four" />
+                                    <input type="radio" name="selected-number" value="4" id="four" onChange={handleChange}/>
                                     <label htmlFor="four">4</label>
-                                    <input type="radio" name="selected-number" value="5" id="five" />
+                                    <input type="radio" name="selected-number" value="5" id="five" onChange={handleChange}/>
                                     <label htmlFor="five">5</label>
-                                    <input type="radio" name="selected-number" value="6" id="six" />
+                                    <input type="radio" name="selected-number" value="6" id="six" onChange={handleChange}/>
                                     <label htmlFor="six">6</label>
                               </div>
                               <h3>Select Number</h3>
                         </div>
                   </div>
                   <div className="roll-dice">
-                        <img src={Cube} alt="Cube" />
+                        <h3>{selectedNumber ? "Selected Number: " + selectedNumber : "Please select a Number" } </h3>
+                        <img src={cube} alt="Cube" onClick={rollDice}/>
                         <h3>Click on Dice to Roll</h3>
                         <button id="reset">Reset Score</button>
                         <button id="rules">Show Rules</button>
