c7b2102478a47c70bf69cade9e904fab86c3f37c
Mahesh Sale <maheshsale333@gmail.com>
2023-11-14 14:08:53 +0530

    Welcome screen ready

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,17 @@
+import './Welcome.css';
+import Image from '../images/dice1.png'
+
 function Welcome(){
       return(
-            <div>Welcome</div>
+            <div className='container'>
+                  <div className="dice1">
+                        <img src={Image} alt="dice1" />
+                  </div>
+                  <div className="title">
+                        <h1>DICE GAME</h1>
+                        <button className='play-btn'>Play Now</button>
+                  </div>
+            </div>
       );
 }
 
