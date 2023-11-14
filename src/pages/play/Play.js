26a7951f84225ab1fa9c41e21c2c91e8cc6de9a7
Mahesh Sale <maheshsale333@gmail.com>
2023-11-15 00:02:02 +0530

    Basic working ready

--- src/pages/play/Play.js
+++ src/pages/play/Play.js
@@ -3,15 +3,26 @@ import './Play.css'
 // import Cube from './images/dice_1.png'
 
 function Play(){
+      const [score, setScore] = useState(0);
+
       const [selectedNumber, setSelectedNumber] = useState(null);
       const [selectedDice, setSelectedDice] = useState(null);
+
+      const [isNumberSelected, setIsNumberSelected] = useState(false);
+      const [isDiceSelected, setIsDiceSelected] = useState(false);
 
       const [cube, setCube] = useState('images/dice_1.png');
       
       let diceArray = ['dice_1', 'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6'];
       
+      if(isNumberSelected && isDiceSelected){
+            selectedNumber === selectedDice ? setScore(score+selectedDice) : setScore(score-selectedDice);
+            //setIsNumberSelected(false);
+            setIsDiceSelected(false);
+      }
       const handleChange = (event) => {
             setSelectedNumber(parseInt(event.target.value));
+            setIsNumberSelected(true);
       }
 
       const getDice = (index) => {
@@ -20,15 +31,24 @@ function Play(){
       }
 
       const rollDice = () => {
-            let randomIndex = Math.floor(Math.random() * 6);
-            setCube(getDice(randomIndex));
-            setSelectedDice(randomIndex + 1);
+            if(isNumberSelected === false){
+                  alert("Select a number first");
+            } else {
+                  let randomIndex = Math.floor(Math.random() * 6);
+                  setCube(getDice(randomIndex));
+                  setSelectedDice(randomIndex + 1);
+                  setIsDiceSelected(true);
+            }
+      }
+
+      const resetScore = () => {
+            setScore(0);
       }
       return(
             <div className="container1">
                   <div className="header">
                         <div className="score">
-                              <h1>0</h1>
+                              <h1>{score}</h1>
                               <p>Total Score</p>
                         </div>
                         <div className="select-number">
@@ -53,7 +73,7 @@ function Play(){
                         <h3>{selectedNumber ? "Selected Number: " + selectedNumber : "Please select a Number" } </h3>
                         <img src={cube} alt="Cube" onClick={rollDice}/>
                         <h3>Click on Dice to Roll</h3>
-                        <button id="reset">Reset Score</button>
+                        <button id="reset" onClick={resetScore}>Reset Score</button>
                         <button id="rules">Show Rules</button>
                   </div>
             </div>
